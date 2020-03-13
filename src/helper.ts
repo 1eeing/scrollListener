1d38a4622b11c1607a7fd3e2944a749c237fbc60
leeing <372711472@qq.com>
2020-03-13 22:51:21 +0800

    bug type

--- src/helper.ts
+++ src/helper.ts
@@ -1,7 +1,7 @@
 export const throttle = (fn: (args: any) => void, delay: number) => {
   let last = 0;
   let timer;
-  return function(...args) {
+  return function(this: any, ...args) {
       const cur = Date.now();
       if(cur - last > delay){
           fn.apply(this, args);
@@ -18,7 +18,7 @@ export const throttle = (fn: (args: any) => void, delay: number) => {
 
 export const debounce = (fn: (args: any) => void, delay: number) => {
   let timer;
-  return function(...args) {
+  return function(this: any, ...args) {
       clearTimeout(timer);
       timer = setTimeout(() => {
           fn.apply(this, args);
